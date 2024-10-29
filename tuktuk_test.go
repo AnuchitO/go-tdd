76362415e5d90f147f29aae48860f3d77e623d4a
Nong AnuchitO <anuchito.code@gmail.com>
2024-10-29 20:08:18 +0700

    refactor: use t.Run to improve readability

--- tuktuk_test.go
+++ tuktuk_test.go
@@ -2,14 +2,16 @@ package main
 
 import "testing"
 
-func TestCalculateFareShouldReturnTotalOf1KmWithoutWaitingTime(t *testing.T) {
-	distance := 1.0
-	waitingTime := 0.0
+func TestCalculateFare(t *testing.T) {
+	t.Run("should return fare of 1 km without waiting time", func(t *testing.T) {
+		distance := 1.0
+		waitingTime := 0.0
 
-	got := calculateFare(distance, waitingTime)
+		got := calculateFare(distance, waitingTime)
 
-	want := 4.0
-	if got != want {
-		t.Errorf("expected %f, got %f", want, got)
-	}
+		want := 4.0
+		if got != want {
+			t.Errorf("expected %f, got %f", want, got)
+		}
+	})
 }
